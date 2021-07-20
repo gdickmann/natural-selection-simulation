e017af55176e7ec9b9e1e13bd30fc04b281cf1f2
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2021-07-20 20:30:08 -0300

    Random hamster color added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-import random
 import pygame
-from pygame.math import Vector2
 from constants.constants import Constants
+from model.hamster import Hamster
 from settings.settings import Settings
 
 pygame.init()
@@ -16,46 +15,6 @@ fps = 60
 
 running = True
 
-class Hamster(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super().__init__()
-
-        self.size = 40
-        self.color = (255, 255, 255)
-        self.tick_size = 1
-
-		# Hamster initial position
-        self.position = Vector2(Constants.HAMSTER_X_POSITION, Constants.HAMSTER_Y_POSITION)
-        # Hamster speed
-        self.speed = Vector2(1.3, 1.3)
-
-        # Set sprite image
-        self.image = pygame.Surface((self.size, self.size), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, self.color, (self.size // 2, self.size // 2),
-                           self.size // 2, self.tick_size)
-
-        self.rect = self.image.get_rect(center=self.position)
-
-    def update(self):
-        self.speed.rotate_ip(random.gauss(0, 1) * 10)
-        self.position += self.speed
-        self.rect.center = self.position
-
-        # Hamster random walk. Thanks, Reddit!
-        if self.rect.left < 0:
-            self.speed.x *= -1
-            self.rect.left = 0
-        elif self.rect.right > width:
-            self.speed.x *= -1
-            self.rect.right = width
-        if self.rect.top < 0:
-            self.speed.y *= -1
-            self.rect.top = 0
-        elif self.rect.bottom > height:
-            self.speed.y *= -1
-            self.rect.bottom = height
-
 # All hamsters!
 all_hamster = pygame.sprite.Group(Hamster() for _ in range(Constants.HAMSTERS))
 
@@ -66,7 +25,7 @@ while running:
 
     all_hamster.update()  # updating hamsters
 
-    screen.fill((51, 51, 51))
+    screen.fill((56, 56, 56))
     all_hamster.draw(screen)  # drawing hamsters on screen
 
     pygame.display.flip()
